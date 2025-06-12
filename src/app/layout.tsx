69a7b21475e3b2f9cec1f5e0fa1650c4aede7298
Stefan Nicolau <contact.sxzar@gmail.com>
2025-06-12 11:20:14 +0300

    feat: add FizziLogo component and Header with styling adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import Header from '@/components/Header';
 import { PrismicPreview } from '@prismicio/next';
 import { repositoryName } from '@/prismicio';
 
@@ -10,7 +11,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="font-alpino">
-            <body>{children}</body>
+            <body className="overflow-x-hidden bg-yellow-300">
+                <Header />
+                <main>{children}</main>
+            </body>
             <PrismicPreview repositoryName={repositoryName} />
         </html>
     );
