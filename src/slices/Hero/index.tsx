b1d63d2dfc4ad3e51a0d03de79254ad60224a7ac
Stefan Nicolau <contact.sxzar@gmail.com>
2025-06-13 15:24:39 +0300

    refactor: remove unused HeroClientAnimation and HeroVisualScene components, integrate their functionality into Hero slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,26 +1,119 @@
+'use client';
+
 import { Content, asText } from '@prismicio/client';
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
 
 import { Bounded } from '@/components/Bounded';
+import { Bubbles } from './Bubbles';
 import Button from '@/components/Button';
 import { FC } from 'react';
-import HeroClientAnimationWrapper from './HeroClientAnimationWrapper';
-import HeroVisualSceneWrapper from './HeroVisualSceneWrapper';
 import { PrismicNextImage } from '@prismicio/next';
+import Scene from './Scene';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { TextSplitter } from '@/components/TextSplitter';
+import { View } from '@react-three/drei';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { useStore } from '@/hooks/useStore';
 
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+/**
+ * Props for `Hero`.
+ */
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
+/**
+ * Component for "Hero" Slices.
+ */
 const Hero: FC<HeroProps> = ({ slice }) => {
+    const ready = useStore((state) => state.ready);
+    const isDesktop = useMediaQuery('(min-width: 768px', true);
+
+    useGSAP(
+        () => {
+            if (!ready && isDesktop) return;
+
+            const introTl = gsap.timeline();
+
+            introTl
+                .set('.hero', { opacity: 1 })
+                .from('.hero-header-word', {
+                    scale: 3,
+                    opacity: 0,
+                    ease: 'power4.in',
+                    delay: 0.3,
+                    stagger: 1
+                })
+                .from(
+                    '.hero-subheading',
+                    {
+                        opacity: 0,
+                        y: 30
+                    },
+                    '+=.8'
+                )
+                .from('.hero-body', {
+                    opacity: 0,
+                    y: 10
+                })
+                .from('.hero-button', {
+                    opacity: 0,
+                    y: 10,
+                    duration: 0.6
+                });
+
+            const scrollTl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.hero',
+                    start: 'top top',
+                    end: 'bottom bottom',
+                    scrub: 1.5
+                }
+            });
+
+            scrollTl
+                .fromTo(
+                    'body',
+                    {
+                        backgroundColor: '#FDE047'
+                    },
+                    {
+                        backgroundColor: '#D9F99D',
+                        overwrite: 'auto'
+                    },
+                    1
+                )
+                .from('.text-side-heading .split-char', {
+                    scale: 1.3,
+                    y: 40,
+                    rotate: -25,
+                    opacity: 0,
+                    stagger: 0.1,
+                    ease: 'back.out(3)',
+                    duration: 0.5
+                })
+                .from('.text-side-body', {
+                    y: 20,
+                    opacity: 0
+                });
+        },
+        { dependencies: [ready, isDesktop] }
+    );
+
     return (
         <Bounded
             data-slice-type={slice.slice_type}
             data-slice-variation={slice.variation}
             className="hero opacity-0"
         >
-            <HeroClientAnimationWrapper />
-            <HeroVisualSceneWrapper />
-
+            {isDesktop && (
+                <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+                    <Scene />
+                    <Bubbles count={300} speed={1} repeat={true} />
+                </View>
+            )}
             <div className="grid">
                 <div className="grid h-screen place-items-center">
                     <div className="grid auto-rows-min place-items-center text-center">
