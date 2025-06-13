fed53db2511f4bc7845f89ed906773b56e7cdf31
Stefan Nicolau <contact.sxzar@gmail.com>
2025-06-13 17:55:56 +0300

    feat: add CircleText component and integrate it into Footer refactor: rename spin-slow class to animate-spin-slow in globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import { PrismicPreview } from '@prismicio/next';
 import ViewCanvas from '@/components/ViewCanvas';
@@ -14,8 +15,11 @@ export default function RootLayout({
         <html lang="en" className="font-alpino">
             <body className="overflow-x-hidden bg-yellow-300">
                 <Header />
-                <main>{children}</main>
-                <ViewCanvas />
+                <main>
+                    {children}
+                    <ViewCanvas />
+                </main>
+                <Footer />
             </body>
             <PrismicPreview repositoryName={repositoryName} />
         </html>
